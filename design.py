f61e4d29e79bc54a3e5d0a4f8a423cc622fde4c7
Alina Andreevna <ali-piratova@mail.ru>
2019-09-22 18:24:09 +0300

    Изменено название окна приложения

--- design.py
+++ design.py
@@ -12,7 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        MainWindow.setObjectName("Serial Code Translator")
         MainWindow.resize(384, 436)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
