c02fc46bccab0e0035800a6d3517069c41d035ba
Alina Andreevna <ali-piratova@mail.ru>
2019-11-02 13:07:53 +0300

    Добавлен новый класс с окном выхода

---
+++ exit.py
@@ -0,0 +1,24 @@
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+
+import exit_dialog
+
+
+class ExitDialog(QtWidgets.QDialog, exit_dialog.Ui_ExitDialog):
+    def __init__(self, parent=None):                                    # Инициализация, наследование
+        super().__init__(parent)
+        self.setupUi(self)
+        self.NoBut.clicked.connect(self.close_exit_dialog)           # Привязываем функцию tex_output к кнопке Code
+        self.YesBut.clicked.connect(self.close_all)       # Привязываем функцию tex_output к кнопке Uncode
+
+    def close_exit_dialog(self):
+        self.close()
+
+    def close_all(self):
+        self.close()
+        sys.exit()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
